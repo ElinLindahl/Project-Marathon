6135b9fd504904e1b4102cd5f5d2b334a4a04982
ElinLindahl <eelinlindahl@gmail.com>
2015-03-20 15:40:48 +0100

    MainController

--- js/MainController.js
+++ js/MainController.js
@@ -1,1 +1,16 @@
-m
+var MainController = function(model) {
+	
+var Searchview = new Searchview($("#Searchview"),model);
+window.SearchviewController = new SearchviewController(Searchview,model);
+
+var CreateNewView = new CreateNewView($("#CreateNewView"),model);
+//window.CreateNewViewController = new CreateNewViewController(CreateNewView,model);
+
+var MarathonListView = new MarathonListView($("#MarathonListView"),model);
+//window.MarathonListViewController = new MarathonListViewController(MarathonListView,model);
+
+var OverviewView = new OverviewView($("#OverviewView"),model);
+//window.OverviewViewController= new OverviewViewController(OverviewView,model);
+
+
+}
