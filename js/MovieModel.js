7b529b53c9bc9f564f8e4e1b40a7ffed2c126992
elvirawiklund <elvira.wiklund@gmail.com>
2015-04-14 17:06:45 +0200

    -

--- js/MovieModel.js
+++ js/MovieModel.js
@@ -1,36 +1,35 @@
 var MovieModel = function() {
-	console.log('Hej');
-this.getmovie = function () {
 	
-	var apikey = "tmaras95gturfua7r8tdvrym";
-	var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+	this.getmovie = function () {
+	
+		var apikey = "tmaras95gturfua7r8tdvrym";
+		var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
 
-	// construct the uri with our apikey
-	var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
-	var query = "Gone with the Wind";
+		// construct the uri with our apikey
+		var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+		var query = "Gone with the Wind";
 
-	$(document).ready(function() {
-		// send off the query
-		$.ajax({
-			url: moviesSearchUrl + '&q=' + encodeURI(query),
-			dataType: "jsonp",
-			success: searchCallback
+		$(document).ready(function() {
+			// send off the query
+			$.ajax({
+				url: moviesSearchUrl + '&q=' + encodeURI(query),
+				dataType: "jsonp",
+				success: searchCallback
 			
+			});
 		});
-	});
 
-	// callback for when we get back the results
-	function searchCallback(data) {
-		$(document.body).append('Found ' + data.total + ' results for ' + query);
-		var movies = data.movies;
- 		console.log(movies);
+		// callback for when we get back the results
+		function searchCallback(data) {
+			$(document.body).append('Found ' + data.total + ' results for ' + query);
+			var movies = data.movies;
+ 			console.log(movies);
  		
-	 	$.each(movies, function(index, movie) {
-	   		$(document.body).append('<h5>' + movie.title + '</h5>');
-	   		$(document.body).append('<img src="' + movie.posters.thumbnail + '" />');
-		});
+	 		$.each(movies, function(index, movie) {
+	   			$(document.body).append('<h5>' + movie.title + '</h5>');
+	   			$(document.body).append('<img src="' + movie.posters.thumbnail + '" />');
+			});
 
+		}
 	}
-}
-	
 }
