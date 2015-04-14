8a349f15e6646591d9bbe6b2afc6cb343d6edaab
elvirawiklund <elvira.wiklund@gmail.com>
2015-04-14 15:42:21 +0200

    Lagt till funktion för att hämta ut en film

--- js/Model.js
+++ js/Model.js
@@ -1,1 +1,34 @@
+var Model = function() {
 
+this.getmovie = function () {
+	
+	var apikey = "tmaras95gturfua7r8tdvrym";
+	var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+
+	// construct the uri with our apikey
+	var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+	var query = "Gone with the Wind";
+
+	$(document).ready(function() {
+		// send off the query
+		$.ajax({
+			url: moviesSearchUrl + '&q=' + encodeURI(query),
+			dataType: "jsonp",
+			success: searchCallback
+		});
+	});
+
+	// callback for when we get back the results
+	function searchCallback(data) {
+		$(document.body).append('Found ' + data.total + ' results for ' + query);
+		var movies = data.movies;
+ 
+	 	$.each(movies, function(index, movie) {
+	   		$(document.body).append('<h1>' + movie.title + '</h1>');
+	   		$(document.body).append('<img src="' + movie.posters.thumbnail + '" />');
+		});
+
+	}
+}
+	
+}
