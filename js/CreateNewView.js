81f155a3de1a38316b905e017d0a41193de2edc4
ElinLindahl <eelinlindahl@gmail.com>
2015-04-15 20:22:23 +0200

    html i viewn

--- js/CreateNewView.js
+++ js/CreateNewView.js
@@ -1,1 +1,29 @@
+var CreateNewView = function (container, Model) {
 
+var Info = container.find("#AllInfo");
+
+var All ='';
+this.getDishes = function ()
+	{
+
+
+	All += "<h3>Create a new movie marathon:</h3></br></br>"
+	All += "<p style='margin-left:3%'>Name:</p><input id='MarathonName' type='text' class='form-control' placeholder='Enter name of the marathon' style='margin-left:3%'>"
+	All += "</br><p style='margin-left:3%'>Place:</p><input id='Place' type='text' class='form-control'  style='margin-left:3%'></br>"
+	All += "<div class='col-xs-3'>"	
+	All += "Date: <input id='Date' type='text' class='form-control'>"
+	All += "Time: <input id='Place' type='text' class='form-control'></div>"
+	All += "<div><span class='input-group-btn'>"
+	All += "<button id='submitbutton' class='btn btn-default' type='button' style='margin-top:35%; margin-left:75%;' >Submit</button>"
+	All += "</span></div>"
+	
+	
+return All
+};
+
+Info.html(this.getDishes());
+
+
+//this.Info.html(getDishes);
+
+}
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(function() {
 	//We instantiate our model
 	var Model = new MovieModel();
 	var searchView = new SearchView($("#SearchView"),Model);
+	var createNewView = new CreateNewView($("#CreateNewView"),Model);
+	
 	
 	//And create the needed controllers and views
 //	var MainController = new MainController(Model);
