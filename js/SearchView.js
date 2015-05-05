51ac69f51a6e09891e6845e0556c8da61ed14e3e
sannano <sannano@kth.se>
2015-05-05 23:23:08 +0200

    Nu går det att söka igen

--- js/SearchView.js
+++ js/SearchView.js
@@ -13,8 +13,8 @@ var SearchView = function (container, Model) {
 			Movies = obj;
 			for(i = 0; i < Movies.length; i++){
 				str += "<li class='moviebox'><div class='movie' id='m"+i+"' onclick='showInfo("+Movies[i].id+")' draggable='true' ondragstart='drag(event)'>";
-				str += "<img src='"+Movies[i].posters.thumbnail+"'>"+Movies[i].title +"";
-				str += "<div id='description"+Movies[i].id+"' class='desc'><table class='padded'><tr><td><img src='"+Movies[i].posters.thumbnail+"'></td><td><p> Runtime: "+Movies[i].runtime+" min </br> Year: "+Movies[i].year+"</br>Audience rating: "+Movies[i].ratings.audience_score+"/100</br>Critics rating: "+Movies[i].ratings.critics_score+"/100</p></td></tr></table></div>";
+				str += "<img src='"+Movies[i].posters.thumbnail+"'></br><h5>"+Movies[i].title +"</h5>";
+				str += "<div id='description"+Movies[i].id+"' class='desc'><table class='padded'><tr><td><p> Runtime: "+Movies[i].runtime+" min </br> Year: "+Movies[i].year+"</br>Audience rating: "+Movies[i].ratings.audience_score+"/100</br>Critics rating: "+Movies[i].ratings.critics_score+"/100</p></td></tr></table></div>";
 				str += "</div></li>";
 			}	
 		}
