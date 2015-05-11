cfd6031fabfc24c863cbc374d5b571fd0c2a45f2
elvirawiklund <elvira.wiklund@gmail.com>
2015-05-11 19:58:16 +0200

    Tillagd startpage

---
+++ js/StartViewController.js
@@ -0,0 +1,13 @@
+var StartViewController = function(view, model) {
+
+$("#startbutton").click(function(){
+		$("#StartView").hide();
+		$("#header").show();
+		$("#SearchView").show();
+		$("#CreateNewView").show();
+});
+
+
+
+
+}
