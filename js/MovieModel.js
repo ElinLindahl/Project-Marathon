06dcfb5bdd1e84eb87e7434dcfb861390e9319e8
elvirawiklund <elvira.wiklund@gmail.com>
2015-05-20 17:04:39 +0200

    Runtime & Endtime helt klara + Lite kommentarer

--- js/MovieModel.js
+++ js/MovieModel.js
@@ -11,7 +11,7 @@ MarathonDate = '';
 MarathonTime = '';
 
 
-
+	
 	this.addObserver = function(observer) {
 		observerlist.push(observer);
 	}
@@ -38,7 +38,7 @@ MarathonTime = '';
 	}
 
 	
-	//Movie by title
+	//Get movies by title
 	this.getmovie = function (query) {
 		var apikey = "tmaras95gturfua7r8tdvrym";
 		var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
@@ -78,36 +78,48 @@ MarathonTime = '';
 			m = Model.getmovie(i);
 			movObjList.push(m);
 		}
-		console.log(movObjList);
-		
+		console.log(movObjList);	
 	}
 	
-	
-		this.getRuntime = function(finalList){
-			var time = 0
-			for (var i=0; i<finalList.length; i++){
-				//i=int(i);
-				time += finalList[i].runtime;
+	//Get total Runtime of your Marathon
+	this.getRuntime = function(finalList){
+		var time = 0
+		for (var i=0; i<finalList.length; i++){
+			//i=int(i);
+			if (finalList[i].runtime==null){
+				time += parseInt(90);
+			}	
+			else {	
+			time += parseInt(finalList[i].runtime);
 			}
-		return time
-		}
+		}	
+	return time
+	}
 
-		
-		this.setEndtime = function(startTime, runTime){
-			var time = startTime;
-			if (time!= null){
-				var dela = time.split('.');
-				var h = dela[0];
-				var min = dela[1];
-				var start = parseInt(h*60)+parseInt(min);
-				var endTime = "";
-				var totalTime = start + runTime;
-				var hours = parseInt(totalTime/60);
-				var minutes = totalTime % 60;
-				endTime = hours + "." + minutes;
-		return endTime
+	//Sets the endtime of the marathon 
+	this.setEndtime = function(startTime, runTime){
+		var time = startTime;
+		if (time!= null){
+			var dela = time.split('.');
+			var h = dela[0];
+			var min = dela[1];
+			var start = parseInt(h*60)+parseInt(min);
+			var endTime = "";
+			var totalTime = start + runTime;
+			var hours = parseInt(totalTime/60);
+			var hoursString = hours.toString();
+			var minutes = totalTime % 60;
+			var minutesString = minutes.toString();
+			if (hoursString.length == 1){
+				hoursString = "0" + hoursString;
 			}
+			if (minutesString.length == 1){
+				minutesString = "0" + minutesString;
+			}
+			endTime = hoursString + "." + minutesString;
+		return endTime
 		}
+	}
 
 		
 	
